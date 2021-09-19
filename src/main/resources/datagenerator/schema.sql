733a8cda549a90c25f58d24a401bf00f6a66dd3b
Tomislav Jurin <svezauzeto12@hotmail.com>
2021-09-19 22:19:15 +0200

    sequences separated from schema

--- src/main/resources/datagenerator/schema.sql
+++ src/main/resources/datagenerator/schema.sql
@@ -1,15 +1,3 @@
-CREATE SEQUENCE user_sequence
-    start 1000
-    increment 1;
-
-CREATE SEQUENCE ticket_sequence
-    start 1000
-    increment 100;
-
-CREATE SEQUENCE match_sequence
-    start 100000
-    increment 1;
-
 drop table if exists users;
 drop table if exists ticket;
 drop table if exists match;
---
+++ src/main/resources/datagenerator/sequence.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE user_sequence
+    start 1000
+    increment 1;
+
+CREATE SEQUENCE ticket_sequence
+    start 1000
+    increment 100;
+
+CREATE SEQUENCE match_sequence
+    start 100000
+    increment 1;
